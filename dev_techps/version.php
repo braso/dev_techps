b2d95a90fd5b3cb95e29893a73b22e595ed66a60
Otávio Augusto C de C <gutsgt1@gmail.com>
2023-10-24 11:57:56 -0300

    Delete .Ds_store, index2, version

--- dev_techps/version.php
+++ dev_techps/version.php
@@ -1,4 +1,4 @@
 <?php 
 
-$version = "1.0.1";
-$release_date = "16-08-2023";
+$version = "1.0.2";
+$release_date = "24-10-2023";
--- version.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-$version = "1.0.1";
-$release_date = "16-08-2023";
