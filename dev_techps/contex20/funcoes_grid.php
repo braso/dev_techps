8065d757303806e2d4c8877528ea422d0cffdc41
Otávio Augusto C de C <gutsgt1@gmail.com>
2023-10-30 12:09:22 -0300

    sincronização + mensagem de rodapé

--- dev_techps/contex20/funcoes_grid.php
+++ dev_techps/contex20/funcoes_grid.php
@@ -23,7 +23,7 @@ function grid2($cabecalho,$valores){
 		echo "<thead><tr>";
 
 		for($i=0;$i<count($cabecalho);$i++){
-			echo "<th>$cabecalho[$i]</th>";
+			echo "<th class='th-align'>$cabecalho[$i]</th>";
 		}
 
 		echo "</thead></tr>";
@@ -44,6 +44,7 @@ function grid2($cabecalho,$valores){
 	}
 
 	echo "</table>";
+	echo "(*): Caso esteja utilizando “Tolerância de jornada Saldo diário (Minutos).";
 	echo '</div>';
 
 	
