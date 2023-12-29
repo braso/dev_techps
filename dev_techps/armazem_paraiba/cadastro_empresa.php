fb1e9cd141a9e20335fece4dcb5f9c81185ac64f
Wallase Alan <39336344+Wallase18@users.noreply.github.com>
2023-12-29 15:47:18 -0300

    feat: lista matriz em primeiro e icone

--- dev_techps/armazem_paraiba/cadastro_empresa.php
+++ dev_techps/armazem_paraiba/cadastro_empresa.php
@@ -81,24 +81,24 @@ function cadastra_empresa(){
 			}
 		}
 // 		$sqlCheckNivel["empr_tx_Ehmatriz"]
-        if(empty($sqlCheckNivel["empr_tx_Ehmatriz"]) || $sqlCheckNivel["empr_tx_Ehmatriz"] != 'Sim'){
-			$campos=[
-				'empr_tx_nome', 'empr_tx_fantasia', 'empr_tx_cnpj', 'empr_tx_cep', 'empr_nb_cidade', 'empr_tx_endereco', 'empr_tx_bairro', 'empr_tx_numero', 'empr_tx_complemento', 'empr_tx_referencia',
-				'empr_tx_fone1', 'empr_tx_fone2', 'empr_tx_email', 'empr_tx_inscricaoEstadual', 'empr_tx_inscricaoMunicipal', 'empr_tx_regimeTributario', 'empr_tx_status', 'empr_tx_situacao', 'empr_nb_parametro', 'empr_tx_contato',
-				'empr_tx_dataRegistroCNPJ', 'empr_tx_domain', 'empr_tx_ftpServer', 'empr_tx_ftpUsername', 'empr_tx_ftpUserpass', 'empr_tx_Ehmatriz'
-			];
+        // if(empty($sqlCheckNivel["empr_tx_Ehmatriz"]) || $sqlCheckNivel["empr_tx_Ehmatriz"] != 'Sim'){
+		// 	$campos=[
+		// 		'empr_tx_nome', 'empr_tx_fantasia', 'empr_tx_cnpj', 'empr_tx_cep', 'empr_nb_cidade', 'empr_tx_endereco', 'empr_tx_bairro', 'empr_tx_numero', 'empr_tx_complemento', 'empr_tx_referencia',
+		// 		'empr_tx_fone1', 'empr_tx_fone2', 'empr_tx_email', 'empr_tx_inscricaoEstadual', 'empr_tx_inscricaoMunicipal', 'empr_tx_regimeTributario', 'empr_tx_status', 'empr_tx_situacao', 'empr_nb_parametro', 'empr_tx_contato',
+		// 		'empr_tx_dataRegistroCNPJ', 'empr_tx_domain', 'empr_tx_ftpServer', 'empr_tx_ftpUsername', 'empr_tx_ftpUserpass', 'empr_tx_Ehmatriz'
+		// 	];
 			
-			$valores = [
-				$_POST['nome'], $_POST['fantasia'], $_POST['cnpj'], $_POST['cep'], $_POST['cidade'], $_POST['endereco'], $_POST['bairro'], $_POST['numero'], $_POST['complemento'], $_POST['referencia'],
-				$_POST['fone1'], $_POST['fone2'], $_POST['email'], $_POST['inscricaoEstadual'], $_POST['inscricaoMunicipal'], $_POST['regimeTributario'], 'ativo', $_POST['situacao'], $_POST['parametro'], $_POST['contato'],
-				$_POST['dataRegistroCNPJ'], "https://braso.mobi/".(is_int(strpos($_SERVER["REQUEST_URI"], 'dev_'))? 'dev_techps/': 'techps/').$_POST['nomeDominio'], $_POST['ftpServer'], $_POST['ftpUsername'], $_POST['ftpUserpass'],
-				$_POST['matriz']
-			];
-		}else {
-		    $campos = ['empr_nb_parametro'];
+		// 	$valores = [
+		// 		$_POST['nome'], $_POST['fantasia'], $_POST['cnpj'], $_POST['cep'], $_POST['cidade'], $_POST['endereco'], $_POST['bairro'], $_POST['numero'], $_POST['complemento'], $_POST['referencia'],
+		// 		$_POST['fone1'], $_POST['fone2'], $_POST['email'], $_POST['inscricaoEstadual'], $_POST['inscricaoMunicipal'], $_POST['regimeTributario'], 'ativo', $_POST['situacao'], $_POST['parametro'], $_POST['contato'],
+		// 		$_POST['dataRegistroCNPJ'], "https://braso.mobi/".(is_int(strpos($_SERVER["REQUEST_URI"], 'dev_'))? 'dev_techps/': 'techps/').$_POST['nomeDominio'], $_POST['ftpServer'], $_POST['ftpUsername'], $_POST['ftpUserpass'],
+		// 		$_POST['matriz']
+		// 	];
+		// }else {
+		//     $campos = ['empr_nb_parametro'];
 			
-			$valores = [$_POST['parametro']];
-		}
+		// 	$valores = [$_POST['parametro']];
+		// }
 
 		$campos = array_merge($campos,array('empr_nb_userAtualiza','empr_tx_dataAtualiza'));
 		$valores = array_merge($valores,array($_SESSION['user_nb_id'], date("Y-m-d H:i:s")));
@@ -173,42 +173,6 @@ function checa_cnpj(){
 	exit;
 }
 
-function campo_domain($nome,$variavel,$modificador,$tamanho,$mascara='',$extra=''){
-
-	if($mascara=="domain") {
-		$data_input="<script>
-			$(document).ready(function() {
-				var inputField = $('#nomeDominio');
-				var domainPrefix = 'https://braso.mobi/".(is_int(strpos($_SERVER["REQUEST_URI"], 'dev_'))? 'dev_techps/': 'techps/')."';
-
-				function updateDisplayedText() {
-					var inputValue = inputField.val();
-
-					if (inputValue.startsWith(domainPrefix)) {
-						var displayedText = inputValue.substring(domainPrefix.length);
-						inputField.val(displayedText);
-					}
-				}
-
-				// Executar a função de atualização quando o campo for modificado
-				inputField.on('input', updateDisplayedText);
-
-				// Inicializar o campo com o valor correto
-				updateDisplayedText();
-			});
-			</script>";
-	}
-
-	$campo='<div class="col-sm-'.$tamanho.' margin-bottom-5">
-			<label><b>'.$nome.'</b></label>
-			<input name="'.$variavel.'" id="'.$variavel.'" value="'.$modificador.'" autocomplete="off" type="text" class="form-control input-sm" '.$extra.'>
-		</div>';
-
-	
-
-	return $campo.$data_input;
-
-}
 
 
 function layout_empresa(){
@@ -536,7 +500,6 @@ function index(){
 	$sql = "SELECT * FROM empresa, cidade WHERE empr_tx_status != 'inativo' AND empr_nb_cidade = cida_nb_id $extra ORDER BY empr_tx_EhMatriz DESC, empr_nb_id";
 	$cab = ['CÓDIGO','NOME','FANTASIA','CPF/CNPJ','CIDADE/UF','SITUAÇÃO','',''];
 	$val = ['empr_nb_id','empr_tx_nome','empr_tx_fantasia','empr_tx_cnpj','concat(cida_nb_id)','empr_tx_situacao','icone_modificar(empr_nb_id,modifica_empresa)','icone_excluir(empr_nb_id,exclui_empresa)'];
-
 	grid($sql,$cab,$val,'','',1,'',10);
 
 	rodape();
