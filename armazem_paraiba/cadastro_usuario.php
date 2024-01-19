c39c16dfb317f9e57cb4ca9823e3d07601c81ce7
Wallase Alan <39336344+Wallase18@users.noreply.github.com>
2024-01-19 16:18:23 -0300

    hotfix: cor botao

--- armazem_paraiba/cadastro_usuario.php
+++ armazem_paraiba/cadastro_usuario.php
@@ -264,7 +264,7 @@
 
 		if( is_int(strpos($_SESSION['user_tx_nivel'], "Super Administrador")) || $_GET['id'] && is_int(strpos($_SESSION['user_tx_nivel'], "Administrador")) || is_int(strpos($_SESSION['user_tx_nivel'], "Funcionário"))){
 			$b = [
-				botao('Gravar', 'cadastra_usuario', 'id,editPermission', $_POST['id'].','.strval($editPermission)),
+				botao('Gravar', 'cadastra_usuario', 'id,editPermission', $_POST['id'].','.strval($editPermission),'','','btn btn-success'),
 				botao('Voltar', 'index')
 			];
 		} else
