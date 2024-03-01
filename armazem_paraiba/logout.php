22b634d3f1de788cfbe4ce9ee63e37a7f678ec65
Otavio ACdeC <gutsgt1@gmail.com>
2024-03-01 14:27:09 -0300

    file

--- armazem_paraiba/logout.php
+++ armazem_paraiba/logout.php
@@ -1,9 +1,7 @@
 <?php
-	include "conecta.php";
-
-	global $CONTEX;
-
+	session_start();
 	$_SESSION = [];
     session_destroy();
+
+	echo "<meta http-equiv='refresh' content='0; url=./../index2.php' />";
 ?>
-<meta http-equiv="refresh" content="0; url=./../index2.php" />
