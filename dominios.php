17456a729943eb1d04ed22a82b8bff7c597de1e8
Otávio Augusto <62617193+GutsGT@users.noreply.github.com>
2024-03-01 14:38:48 -0300

    file (#139)

--- dominios.php
+++ dominios.php
@@ -1,32 +1,31 @@
-<?php 
-
+<?php
 if(isset($_SERVER['SCRIPT_URI'])){
 	$server_base_link = substr($_SERVER['SCRIPT_URI'], 0, strrpos($_SERVER['SCRIPT_URI'], '/'));
 }else{
 	$server_base_link = substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/'));
 }
-include $server_base_link."/armazem_paraiba/conecta.php";
 
-$dominio_array = [
-    "techps",
-    "braso",
-    "armazem_paraiba",
-    "opafrutas",
-    "qualy_transportes",
-    "feijao_turqueza"
+$dominios = [
+    "techps"            => 'Techps',
+    "braso"             => 'Braso',
+    "armazem_paraiba"   => 'Armazem Paraíba', 
+    "opafrutas"         => 'Opafrutas',
+    "qualy_transportes" => 'Qualy Transportes',
+    "feijao_turqueza"   => 'Feijão Turqueza'
 ];
 
-$dominios = "<div class='form-group'>
+$dominiosInput = "<div class='form-group'>
     <select class='form-control' name='dominio'>
-        <option value='' selected>Domínio</option>
-        <option value='" . $server_base_link . "/techps/index.php'>Techps</option>
-        <option value='" . $server_base_link . "/braso/index.php'>Braso</option>
-        <option value='" . $server_base_link . "/armazem_paraiba/index.php'>Armazem Paraiba</option>
-        <option value='" . $server_base_link . "/opafrutas/index.php'>Opafrutas</option>
-        <option value='" . $server_base_link . "/qualy_transportes/index.php'>Qualy Transportes</option>
-        <option value='" . $server_base_link . "/feijao_turqueza/index.php'>Feijão turqueza</option>
-        <option value=''>Leroy Merlin</option>
-    </select>
-</div>";
+        <option value='' hidden selected>Domínio</option>";
+foreach($dominios as $key => $value){
+    if(!empty($_POST['dominio']) && $_POST['dominio'] == $server_base_link."/".$key."/index.php"){
+        $selected = 'selected';
+    }else{
+        $selected = '';
+    }
 
+    $dominiosInput .= "<option value='".$server_base_link."/".$key."/index.php' ".$selected.">".$value."</option>";
+}
+
+$dominiosInput .= "</select></div>";
 ?>
