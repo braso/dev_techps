e4f72299f67da147c061c1ad53da622629a579cd
Wallase Alan <39336344+Wallase18@users.noreply.github.com>
2024-03-05 14:26:33 -0300

    feat:print espelho

--- armazem_paraiba/nao_conformidade.php
+++ armazem_paraiba/nao_conformidade.php
@@ -64,11 +64,20 @@
 				array_unshift($c, combo_net('Empresa*:', 'busca_empresa',   (!empty($_POST['busca_empresa'])?   $_POST['busca_empresa']  : ''), 3, 'empresa', 'onchange=selecionaMotorista(this.value)', $extraEmpresa));
 			}
 		//}
+		$botao_imprimir =
+			'<button class="btn default" type="button" onclick="imprimir()">Imprimir</button>
+			<script>
+    			function imprimir() {
+    			// Abrir a caixa de diálogo de impressão
+    			window.print();
+    			}
+			</script>';
 
 		//BOTOES{
 			$b = [
 				botao("Buscar", 'index', '', '', '', 1,'btn btn-success'),
 				botao("Cadastrar Abono", 'layout_abono', '', '', '', 1),
+				$botao_imprimir,
 				'<span id=dadosResumo><b>'.$carregando.'</b></span>'
 			];
 		//}
@@ -76,6 +85,17 @@
 		abre_form('Filtro de Busca');
 		linha_form($c);
 		fecha_form($b);
+		
+		?>
+		<div id="tituloRelatorio">
+			<h1>Não Conformidade</h1>
+		</div>
+		<style>
+			#tituloRelatorio{
+			    display: none;
+    		}
+		</style>
+		<?php
 
 		$cab = [
 			"", "DATA", "DIA", "INÍCIO JORNADA", "INÍCIO REFEIÇÃO", "FIM REFEIÇÃO", "FIM JORNADA",
@@ -287,6 +307,69 @@
 			}
 		//}
 		echo '<div class="printable"></div>';
+		
+		?>
+
+	<style>
+
+			@media print {
+    		        body {
+                        margin: 1cm;
+                        margin-right: 0cm; /* Ajuste o valor conforme necessário para afastar do lado direito */
+                        transform: scale(1.0);
+                        transform-origin: top left;
+                    }
+                
+                    @page {
+                        size: A4 landscape;
+                        margin: 1cm;
+                    }
+                    #tituloRelatorio{
+                        display: block; /* Torna visível apenas ao imprimir */
+                        font-size: 12px;
+                        padding-left: 500px;
+                    }
+                    body > div.scroll-to-top{
+                        display: none !important;
+                    }
+                    body > div.page-container > div > div.page-content > div > div > div > div > div:nth-child(5){
+                        display: none;
+                    }
+                    .portlet-body.form .table-responsive {
+                        overflow-x: visible !important;
+                        margin-left: -50px !important;
+                    }
+                    .portlet.light>.portlet-title {
+                        border-bottom: none;
+                        margin-bottom: 0px;
+                    }
+                    .caption{
+                        padding-top: 0px;
+                        margin-left: -50px !important;
+                        padding-bottom: 0px;
+                    }
+            }
+			#saldo {
+				width: 50% !important;
+				margin-top: 9px !important;
+				text-align: center;
+			}
+		
+				table thead tr th:nth-child(3),
+				table thead tr th:nth-child(7),
+				table thead tr th:nth-child(11),
+				table td:nth-child(3),
+				table td:nth-child(7),
+				table td:nth-child(11) {
+					border-right: 3px solid #d8e4ef !important;
+				}
+				.th-align {
+				    text-align: center; /* Define o alinhamento horizontal desejado, pode ser center, left ou right */
+				    vertical-align: middle !important; /* Define o alinhamento vertical desejado, pode ser top, middle ou bottom */
+				    
+				}
+			</style>
+		<?
 
 		rodape();
 
