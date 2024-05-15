17fddcdc16efac00818e345babc0bf934cb73c10
Otavio ACdeC <gutsgt1@gmail.com>
2024-05-15 15:08:38 -0300

    ./contex20/select2.php

--- contex20/select2.php
+++ contex20/select2.php
@@ -9,13 +9,14 @@
 	// GLOBAL $CONTEX;
 	$interno = true; //Utilizado no conecta.php para reconhecer se quem está tentando acessar é uma tela ou uma query interna.
 	include_once "../..".$_GET['path']."/conecta.php";
-	GLOBAL $conn;
 
+	GLOBAL $conn;
+	
 	$tabela 	  = $_GET['tabela'];
 	$tab 		  = substr($_GET['tabela'],0,4);
 	$extra_bd 	  = urldecode($_GET['extra_bd']);
 	$extra_busca  = urldecode($_GET['extra_busca']);
-
+	
 	$extra = " AND (".$tab."_tx_nome LIKE '%".$_GET['q']."%'";
 	if(!empty($extra_busca)){
 		$extra_campo = ",".$extra_busca;
@@ -24,24 +25,25 @@
 		$extra_campo = "";
 		$extra .= ")";
 	}
-
+	
 	if($tabela == 'servico' && $_GET['path'] == '/imagem'){
 		$sql = 
-			"SELECT ".$tab."_nb_id,CONCAT(".$tab."_tx_nome,' | ',".$tab."_tx_tipo) AS ".$tab."_tx_nome FROM ".$tabela." 
-				WHERE ".$tab."_tx_nome LIKE '%".$_GET['q']."%'"; 
+		"SELECT ".$tab."_nb_id,CONCAT(".$tab."_tx_nome,' | ',".$tab."_tx_tipo) AS ".$tab."_tx_nome FROM ".$tabela." 
+		WHERE ".$tab."_tx_nome LIKE '%".$_GET['q']."%'"; 
 	}else{
 		$sql = 
-			"SELECT ".$tab."_nb_id, ".$tab."_tx_nome ".(!empty($extra_busca)? ",".$extra_busca: "")." FROM ".$tabela." 
-				WHERE 1 $extra"
+		"SELECT ".$tab."_nb_id, ".$tab."_tx_nome ".(!empty($extra_busca)? ",".$extra_busca: "")." FROM ".$tabela." 
+		WHERE 1 $extra"
 		;
 	}
-
+	
 	$sql .= " AND ".$tab."_tx_status != 'inativo' 
 			$extra_bd
 		".(!empty(urldecode($_GET['extra_ordem']))? urldecode($_GET['extra_ordem']): " ORDER BY ".$tab."_tx_nome ASC")." 
 		".(!empty(urldecode($_GET['extra_limite']))? " LIMIT ".urldecode($_GET['extra_limite']): '').";"
 	;
 
+	
 	$result = mysqli_fetch_all(
 		query($sql),
 		MYSQLI_ASSOC
@@ -49,6 +51,7 @@
 
 	$json = [];
 	foreach($result as $row){
+		$extra_exibe = "";
 		if($extra_busca != ''){
 			$extra_exibe = "[".$row[$extra_busca]."]";
 		}
