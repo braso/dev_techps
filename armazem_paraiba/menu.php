dca03d870b90d8a0aba8cf1ea56c6385e6bbbc74
Wallase Alan <39336344+Wallase18@users.noreply.github.com>
2024-06-14 10:54:57 -0300

    Hotfix/wallase (#203)
    * fix:relatorios ajuste e painel
    
    * fix:carregar ponto
    
    * remocoa de vinculo de motivo batida de ponto
    
    * adicao de butao imprimir para cada endosso e ajuste de css
    
    * ajuste de css
    
    * ajuste de painel endosso
    
    * ajustes painel
    
    * hotfix painel

--- armazem_paraiba/menu.php
+++ armazem_paraiba/menu.php
@@ -56,7 +56,11 @@ if (is_int(strpos($_SESSION['user_tx_nivel'], 'Administrador')) || is_int(strpos
 						</ul>
 					</li>
 					<li class="menu-dropdown classic-menu-dropdown <?= verificarAtividade($paginaPainel) ?>">
-						<a href="<?= $CONTEX["path"]?>/painel.php">Painel<span class="arrow"></span></a>
+						<a href="javascript:;">Painel<span class="arrow"></span></a>
+							<ul class="dropdown-menu pull-left">
+								<li class=""><a href="<?= $CONTEX["path"] ?>/painel.php" class="nav-link ">Endossos</a></li>
+								<li class=""><a href="<?= $CONTEX["path"] ?>/painel_nao_conformidade.php" class="nav-link">Não Conformidade</a></li>
+							</ul>		
 					</li>
 					<?php if(is_int(strpos($_SERVER["REQUEST_URI"], 'dev_'))){ ?>
 					<li class="menu-dropdown classic-menu-dropdown ">
