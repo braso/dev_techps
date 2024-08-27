6235f4ee531bbf062cbf39a28134731c987b0a44
GutsGT <gutsgt1@gmail.com>
2024-08-27 15:35:54 -0300

    armazem_paraiba/paineis/painel_saldo_html.php

--- armazem_paraiba/paineis/painel_saldo_html.php
+++ armazem_paraiba/paineis/painel_saldo_html.php
@@ -1,219 +1,140 @@
-<link rel='stylesheet' href='<?=$_ENV["APP_PATH"].$_ENV["CONTEX_PATH"]."/css/paineis.css"?>'>
-<div id='tituloRelatorio'>
-	<img style='width: 150px' src='<?=$_SERVER["HTTP_ORIGIN"].$_ENV["APP_PATH"].$_ENV["CONTEX_PATH"]."/".$aEmpresa[0]["empr_tx_logo"]?>' alt='Logo Empresa Esquerda'>
+<link rel="stylesheet" href="../css/paineis.css">
+<div id="printTitulo">
+	<img style='width: 150px' src="<?= $aEmpresa[0]['empr_tx_logo']?>" alt="Logo Empresa Esquerda">
 	<h3>Relatorio Geral de saldo</h3>
-	<div class='right-logo'>
-		<img style='width: 150px' src='<?=$CONTEX["path"]?>/imagens/logo_topo_cliente.png' alt='Logo Empresa Direita'>
+	<div class="right-logo">
+		<img style='width: 150px' src="<?= $CONTEX['path']?>/imagens/logo_topo_cliente.png" alt="Logo Empresa Direita">
 	</div>
 </div>
-<div class='col-md-12 col-sm-12' id='pdf2htmldiv'>
-	<div class='portlet light '>
-		<div class='table-responsive'>
-			<div style='display: flex;'>
+<div class="col-md-12 col-sm-12" id="pdf2htmldiv">
+	<div class="portlet light ">
+		<div class="table-responsive">
+			<div>
 				<table class='table w-auto text-xsmall table-bordered table-striped table-condensed flip-content table-hover compact' id='tabela1'>
                     <thead>
                         <tr>
                             <th colspan='1'></th>
-                            <th colspan='1'>TOTAL</th>
+                            <th colspan='1'>QUANT</th>
                             <th colspan='1'>%</th>
                         </tr>
                     </thead>
                     <tbody>
 						<tr>
 							<td>NÃO ENDOSSADO</td>
+							<td></td>
 							<td class='porcentagemNaEndo'></td>
-							<td class="porcentagemNaEndo"></td>
 						</tr>
 						<tr>
 							<td>ENDOSSO PARCIAL</td>
+							<td></td>
 							<td class='porcentagemEndoPc'></td>
-							<td class="porcentagemEndoPc"></td>
 						</tr>
 						<tr>
 							<td>ENDOSSADO</td>
+							<td></td>
 							<td class='porcentagemEndo'></td>
-							<td class="porcentagemEndo"></td>
 						</tr>
                     </tbody>
                 </table>
-				
-				<table class='table w-auto text-xsmall table-bordered table-striped table-condensed flip-content table-hover compact' id='tabela2'>
-					<thead>
-						<tr>
-							<th colspan='1'>SALDO FINAL</th>
-							<th colspan='1'>TOTAL</th>
-							<th colspan='1'>%</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td class='porcentagemMeta'>META</td>
-							<td class='textCentralizado'><?=$quantMeta?></td>
-							<td><?=$performance["meta_endo"]?></td>
-						</tr>
-						<tr>
-							<td class='porcentagemPosit'>POSITIVO</td>
-							<td class='textCentralizado'><?=$quantPosi?></td>
-							<td><?=$performance["posi_endoPc"]?></td>
-						</tr>
-						<tr>
-							<td class='porcentagemNegat'>NEGATIVO</td>
-							<td class='textCentralizado'><?=$quantNega?></td>
-							<td><?=$performance["nega_naEndo"]?></td>
-						</tr>
-					</tbody>
-				</table>
+                <table class='table w-auto text-xsmall table-bordered table-striped table-condensed flip-content table-hover compact' id='tabela2'>
+                    <thead>
+                        <tr>
+                            <th colspan='1'>SALDO FINAL</th>
+                            <th colspan='1'>QUANT</th>
+                            <th colspan='1'>%</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class='porcentagemMeta'>
+                            <td>META</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr class='porcentagemPosi'>
+                            <td>POSITIVO</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr class='porcentagemNega'>
+                            <td>NEGATIVO</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
 				<div class='emissao'>
-					<?=(!empty($emissao)? "<b>Atualizado em:</b> ".$emissao."<br>": "")
-					."<b>Período do relatório:</b> ".$dataInicio->format("d/m")." a ".$dataFim->format("d/m")?></div>
+					<?=(!empty($dataEmissao)? "<b>Atualizado em:</b> ".$dataEmissao."<br>": "")
+					."<b>Período do relatório:</b> ".$periodoRelatorio["dataInicio"]." a ".$periodoRelatorio["dataFim"]?>
+				</div>
 			</div>
-			<br>
-			<div class='portlet-body form'>
-				<table id='tabela-conteudo' class='table w-auto text-xsmall table-bordered table-striped table-condensed flip-content table-hover compact'>
+			<div class="portlet-body form">
+				<table id="tabela-empresas" class="table w-auto text-xsmall table-bordered table-striped table-condensed flip-content table-hover compact">
 					<thead>
-						<tr class='totais'>
-							<th colspan='2'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
-							<th colspan='1'></th>
+						<tr class="totais">
+							<th colspan="1">Período: De <?=$periodoRelatorio["dataInicio"].' até '.$periodoRelatorio["dataFim"]?></th>
+							<th></th>
+							<?php
+							if (!empty($_POST['empresa'])){
+								echo "<th colspan='1'>".$totais['empresaNome']."</th>"
+									."<th colspan='1'>".$totais['jornadaPrevista']."</th>"
+									."<th colspan='1'>".$totais['jornadaEfetiva']."</th>"
+									."<th colspan='1'>".$totais['HESemanal']."</th>"
+									."<th colspan='1'>".$totais['HESabado']."</th>"
+									."<th colspan='1'>".$totais['adicionalNoturno']."</th>"
+									."<th colspan='1'>".$totais['esperaIndenizada']."</th>"
+									."<th colspan='1'>".$totais['saldoAnterior']."</th>"
+									."<th colspan='1'>".$totais['saldoPeriodo']."</th>"
+									."<th colspan='1'>".$totais['saldoFinal']."</th>";
+							} else {
+								echo "<th colspan='1'>".$totais["jornadaPrevista"]."</th>"
+									."<th colspan='1'>".$totais["jornadaEfetiva"]."</th>"
+									."<th colspan='1'> ".(($totais['HESemanal'] == '00:00')? '': $totais['HESemanal'])."</th>"
+									."<th colspan='1'> ".(($totais['HESabado'] == '00:00')? '': $totais['HESabado'])."</th>"
+									."<th colspan='1'> ".(($totais['adicionalNoturno'] == '00:00')? '': $totais['adicionalNoturno'])."</th>"
+									."<th colspan='1'> ".(($totais['esperaIndenizada'] == '00:00')? '': $totais['esperaIndenizada'])."</th>"
+									."<th colspan='1'> ".(($totais['saldoAnterior'] == '00:00')? '': $totais['saldoAnterior'])."</th>"
+									."<th colspan='1'> ".(($totais['saldoPeriodo'] == '00:00')? '': $totais['saldoPeriodo'])."</th>"
+									."<th colspan='1'> ".(($totais['saldoFinal'] == '00:00')? '': $totais['saldoFinal'])."</th>";
+							}
+							?>
 						</tr>
-						<tr id='titulos' class='titulos'>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
-							<th data-order='asc'></th>
+						<tr id='titulos' class="titulos">
+							<?=((!empty($_POST['empresa']))?
+								 	"<th class='matricula'></th>"
+									."<th class='nome'></th>"
+									."<th class='status'></th>"
+									."<th class='jornadaPrevista'></th>"
+									."<th class='jornadaEfetiva'></th>"
+									."<th class='HESemanal'></th>"
+									."<th class='HESabado'></th>"
+									."<th class='adicionalNoturno'></th>"
+									."<th class='esperaIndenizada'></th>"
+									."<th class='saldoAnterior'></th>"
+									."<th class='saldoPeriodo'></th>"
+									."<th class='saldoFinal'></th>"
+
+									:"<th data-column='nome' data-order='asc'>Todos os CNPJ</th>"
+									."<th data-column='qtdMotoristas' data-order='asc'>Quant. Motoristas</th>"
+									."<th data-column='jornadaPrevista' data-order='asc'>Jornada Prevista</th>"
+									."<th data-column='JornadaEfetiva' data-order='asc'>Jornada Efetiva</th>"
+									."<th data-column='HESemanal' data-order='asc'>HE 50%</th>"
+									."<th data-column='HESabado' data-order='asc'>HE 100%</th>"
+									."<th data-column='adicionalNoturno' data-order='asc'>Adicional Noturno</th>"
+									."<th data-column='esperaIndenizada' data-order='asc'>ESPERA INDENIZADA</th>"
+									."<th data-column='saldoAnterior' data-order='asc'>Saldo Anterior</th>"
+									."<th data-column='saldoPeriodo' data-order='asc'>Saldo Periodo</th>"
+									."<th data-column='saldoFinal' data-order='asc'>Saldo Final</th>"
+							)?>
 						</tr>
 					</thead>
 					<tbody>
-						<!-- Conteúdo do json -->
+						<!-- Conteúdo do json empresas será inserido aqui -->
 					</tbody>
 				</table>
 			</div>
 		</div>
 	</div>
 </div>
-<div id='impressao'>
-	<b>Impressão Doc.:</b> <?=date("d/m/Y \T H:i:s")." (UTC-3)"?>
+<div id="impressao">
+	<b>Impressão Doc.:</b> <?= date("d/m/Y \T H:i:s")." (UTC-3)"?>
 </div>
-<script>
-	$(document).ready(function() {
-		var tabela = $('#tabela-conteudo tbody');
-		function carregarDados() {
-			$.ajax({
-				url: '<?=$url?>',
-				dataType: 'json',
-				success: function(data) {
-					tabela.empty();
-					$.each(data, function(index, item) {
-						//console.log("Item "+index+":");
-						// for (var chave in item) {
-						// 	if (item.hasOwnProperty(chave)) {
-								//console.log(chave+": "+item[chave]);
-						// 	}
-						// }
-						// console.log(item);
-
-						<?php
-							$linha = "'<tr>'";
-							if (!empty($_POST['empresa']) && !empty($_POST['busca_dataInicio']) && !empty($_POST['busca_dataFim'])){
-								$linha .=
-									 "+'<td>'+item.matricula+'</td>'"
-									."+'<td>'+item.motorista+'</td>'";
-							} else {
-								$linha .= 
-									 "+'<td style=\"cursor: pointer;\" onclick=setAndSubmit('+item.empresaId+')>'+item.empresaNome+'</td>'"
-									."+'<td>'+item.totalMotorista+'</td>'"
-								;
-							}
-							$linha .= 
-								 "+'<td>'+item.statusEndosso+'</td>'"
-								."+'<td>'+item.jornadaPrevista+'</td>'"
-								."+'<td>'+item.jornadaEfetiva+'</td>'"
-								."+'<td>'+((item.he50 === null || item.he50 === '00:00')? '': item.he50)+'</td>'"
-								."+'<td>'+((item.he100 === null || item.he100 === '00:00')? '': item.he100)+'</td>'"
-								."+'<td>'+((item.adicionalNoturno === null || item.adicionalNoturno === '00:00')? '': item.adicionalNoturno)+'</td>'"
-								."+'<td>'+((item.esperaIndenizada === null || item.esperaIndenizada === '00:00')? '': item.esperaIndenizada)+'</td>'"
-								."+'<td>'+((item.saldoAnterior === null || item.saldoAnterior === '00:00')? '': item.saldoAnterior)+'</td>'"
-								."+'<td>'+((item.saldoPeriodo === null || item.saldoPeriodo === '00:00')? '': item.saldoPeriodo)+'</td>'"
-								."+'<td>'+((item.saldoFinal === null || item.saldoFinal === '00:00')? '': item.saldoFinal)+'</td>'";
-							
-							$linha .= "+'</tr>'";
-							echo "var linha = ".$linha.";";
-						?>
-						tabela.append(linha);
-					});
-				},
-				error: function() {
-					console.log('Erro ao carregar os dados.');
-				}
-			});
-		}
-		// Função para ordenar a tabela
-		function ordenarTabela(coluna, ordem) {
-			var linhas = tabela.find('tr').get();
-			linhas.sort(function(a, b) {
-				var valorA = $(a).children('td').eq(coluna).text().toUpperCase();
-				var valorB = $(b).children('td').eq(coluna).text().toUpperCase();
-
-				if(valorA != valorB){
-					return ((ordem === 'asc'? -1: 1)*((valorA < valorB)? 1: -1));
-				}
-				return 0;
-			});
-			$.each(linhas, function(index, row) {
-				tabela.append(row);
-			});
-		}
-
-		// Evento de clique para ordenar a tabela ao clicar no cabeçalho
-		$('#titulos th').click(function() {
-			var coluna = $(this).index();
-			var ordem = $(this).data('order');
-			$('#tabela-conteudo th').data('order', 'desc'); // Redefinir ordem de todas as colunas
-			$(this).data('order', (ordem === 'desc'? 'asc': 'desc'));
-			ordenarTabela(coluna, $(this).data('order'));
-
-			// Ajustar classes para setas de ordenação
-			$('#titulos th').removeClass('sort-asc sort-desc');
-			$(this).addClass('sort-'+($(this).data('order') === 'asc'? 'asc': 'desc'));
-		});
-
-		carregarDados();
-	});
-
-	// function downloadCSV(nomeCsv) {
-	//     // Caminho do arquivo CSV no servidor
-	//     var filePath = './arquivos/paineis/Painel_'+nomeCsv+'.csv' // Substitua pelo caminho do seu arquivo
-
-	//     // Cria um link para download
-	//     var link = document.createElement('a');
-
-	//     // Configurações do link
-	//     link.setAttribute('href', filePath);
-	//     link.setAttribute('download', 'Painel_'+nomeCsv+'.csv');
-
-	//     // Adiciona o link ao documento
-	//     document.body.appendChild(link);
-
-	//     // Simula um clique no link para iniciar o download
-	//     link.click();
-
-	//     // Remove o link
-	//     document.body.removeChild(link);
-	// }
-</script>
