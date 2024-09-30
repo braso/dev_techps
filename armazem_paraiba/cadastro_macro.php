55c08c8800f0c77571c0da6e926f55aa955b583e
Otavio ACdeC <gutsgt1@gmail.com>
2024-09-30 09:19:53 -0300

    ./armazem_paraiba/cadastro_macro.php

--- armazem_paraiba/cadastro_macro.php
+++ armazem_paraiba/cadastro_macro.php
@@ -1,103 +1,127 @@
 <?php
-include "conecta.php";
+	include "conecta.php";
 
-function exclui_macro(){
+	function exclui_macro(){
 
-	remover('macroponto',$_POST['id']);
-	index();
-	exit;
+		remover("macroponto",$_POST["id"]);
+		index();
+		exit;
 
-}
-function modifica_macro(){
-	global $a_mod;
-
-	$a_mod=carregar('macroponto',$_POST['id']);
-
-	layout_macro();
-	exit;
-
-}
-
-function cadastra_macro(){
-	
-	// $campos=array(macr_tx_nome,macr_tx_codigoInterno,macr_tx_codigoExterno,macr_nb_user,macr_tx_data,macr_tx_status);
-	// $valores=array($_POST[nome],$_POST[codigoInterno],$_POST[codigoExterno],$_SESSION[user_nb_id],date("Y-m-d"),'ativo');
-	$campos=array('macr_tx_codigoExterno','macr_nb_user','macr_tx_data','macr_tx_status');
-	$valores=array($_POST['codigoExterno'],$_SESSION['user_nb_id'],date("Y-m-d"),'ativo');
-
-	if($_POST['id']>0){
-		atualizar('macroponto',$campos,$valores,$_POST['id']);
 	}
-	// else
-	// 	inserir('macroponto',$campos,$valores);
+	function modifica_macro(){
+		$_POST = array_merge($_POST, carregar("macroponto",$_POST["id"]));
 
-	index();
-	exit;
-}
+		layout_macro();
+		exit;
+	}
 
+	function cadastra_macro(){
 
-function layout_macro(){
-	global $a_mod;
-
-	cabecalho("Cadastro Macro");
-
-	$c = [
-		campo('Nome','nome',$a_mod['macr_tx_nome'],6,'','readonly=readonly'),
-		campo('Código Interno','codigoInterno',$a_mod['macr_tx_codigoInterno'],3,'','readonly=readonly'),
-		campo('Código Externo','codigoExterno',$a_mod['macr_tx_codigoExterno'],3)
-	];
-
-	$botao = [
-		botao('Gravar','cadastra_macro','id',$_POST['id'],'','','btn btn-success'),
-		botao('Voltar','index')
-	];
-
-	if(empty($_POST["HTTP_REFERER"])){
-		$_POST["HTTP_REFERER"] = $_SERVER["HTTP_REFERER"];
-		if(is_int(strpos($_SERVER["HTTP_REFERER"], "cadastro_macro.php"))){
-			$_POST["HTTP_REFERER"] = $_ENV["URL_BASE"].$_ENV["APP_PATH"].$_ENV["CONTEX_PATH"]."/cadastro_macro.php";
+		$camposObrig = [
+			"nome" => "Nome", 
+			"codigoInterno" => "Código Interno", 
+			"codigoExterno" => "Código Externo"
+		];
+		$errorMsg = "ERRO: Campos obrigatórios não preenchidos: ";
+		foreach($camposObrig as $key => $value){
+			if(empty($_POST[$key])){
+				$_POST["errorFields"][] = $key;
+				$errorMsg .= $value.", ";
+			}
 		}
+
+		if(!empty($_POST["errorFields"])){
+			set_status(substr($errorMsg, 0, -2).".");
+			layout_macro();
+			exit;
+		}
+
+		$novaMacro = [
+			"macr_tx_nome" => $_POST["nome"],
+			"macr_tx_codigoInterno" => $_POST["codigoInterno"],
+			"macr_tx_codigoExterno" => $_POST["codigoExterno"],
+			"macr_nb_user" => $_SESSION["user_nb_id"],
+			"macr_tx_data" => date("Y-m-d"),
+			"macr_tx_status" => "ativo"
+		];
+
+		dd("teste");
+		if(!empty($_POST["id"])){
+			atualizar("macroponto",array_keys($novaMacro), array_values($novaMacro),$_POST["id"]);
+		}else{
+			inserir("macroponto",array_keys($novaMacro), array_values($novaMacro));
+		}
+
+		index();
+		exit;
 	}
-	
-	abre_form('Dados do Macro');
-	echo campo_hidden("HTTP_REFERER", $_POST["HTTP_REFERER"]);
-	linha_form($c);
-	fecha_form($botao);
 
-	rodape();
 
-}
+	function layout_macro(){
 
-function index(){
+		cabecalho("Cadastro Macro");
 
-	cabecalho("Cadastro Macro");
-	
-	$extra = '';
+		$c = [
+			campo("Nome*","nome",($_POST["macr_tx_nome"]?? ""),6,"","readonly=readonly"),
+			campo("Código Interno*","codigoInterno",($_POST["macr_tx_codigoInterno"]?? ""),3,"","readonly=readonly"),
+			campo("Código Externo*","codigoExterno",($_POST["macr_tx_codigoExterno"]?? ""),3)
+		];
 
-	if($_POST['busca_codigo'])
-		$extra .= " AND macr_nb_id = '$_POST[busca_codigo]'";
+		$botao = [
+			// botao("Gravar","cadastra_macro","id",$_POST["id"],"","","btn btn-success"),
+			botao("Voltar","index")
+		];
 
-	if($_POST['busca_nome'])
-		$extra .= " AND macr_tx_nome LIKE '%$_POST[busca_nome]%'";
+		if(empty($_POST["HTTP_REFERER"])){
+			$_POST["HTTP_REFERER"] = $_SERVER["HTTP_REFERER"];
+			if(is_int(strpos($_SERVER["HTTP_REFERER"], "cadastro_macro.php"))){
+				$_POST["HTTP_REFERER"] = $_ENV["URL_BASE"].$_ENV["APP_PATH"].$_ENV["CONTEX_PATH"]."/cadastro_macro.php";
+			}
+		}
+		
+		abre_form("Dados do Macro");
+		echo campo_hidden("HTTP_REFERER", $_POST["HTTP_REFERER"]);
+		linha_form($c);
+		fecha_form($botao);
 
-	$c = [
-		campo('Código','busca_codigo',$_POST['busca_codigo'],2,'MASCARA_NUMERO'),
-		campo('Nome','busca_nome',$_POST['busca_nome'],10)
-	];
+		rodape();
 
-	$botao[] = botao('Buscar','index');
-	// $botao[] = botao('Inserir','layout_macro');
-	
-	abre_form('Filtro de Busca');
-	linha_form($c);
-	fecha_form($botao);
+	}
 
-	$sql = "SELECT * FROM macroponto WHERE macr_tx_status = 'ativo' $extra";
-	$cab = array('CÓDIGO','NOME','CÓD. INTERNO','CÓD. EXTERNO','');
-	$val = array('macr_nb_id','macr_tx_nome','macr_tx_codigoInterno','macr_tx_codigoExterno','icone_modificar(macr_nb_id,modifica_macro)');
+	function index(){
 
-	grid($sql,$cab,$val,'','',0,'desc',10);
+		cabecalho("Cadastro Macro");
+		
+		$extra = 
+			(($_POST["busca_codigo"])? " AND macr_nb_id = ".$_POST["busca_codigo"]: "")
+			.(($_POST["busca_nome"])? " AND macr_tx_nome LIKE '%".$_POST["busca_nome"]."%'": "")
+		;
 
-	rodape();
+		$campos = [
+			campo("Código","busca_codigo",$_POST["busca_codigo"],2,"MASCARA_NUMERO"),
+			campo("Nome","busca_nome",$_POST["busca_nome"],10)
+		];
 
-}
+		$botoes = [
+			botao("Buscar","index"),
+			// botao("Inserir","layout_macro")
+		];
+		
+		abre_form();
+		linha_form($campos);
+		fecha_form($botoes);
+
+		$sql = "SELECT * FROM macroponto WHERE macr_tx_status = 'ativo' ".$extra;
+		$cols = [
+			"CÓDIGO" => "macr_nb_id",
+			"NOME" => "macr_tx_nome",
+			"CÓD. INTERNO" => "macr_tx_codigoInterno",
+			"CÓD. EXTERNO" => "macr_tx_codigoExterno",
+			"<spam class='glyphicon glyphicon-search'></spam>" => "icone_modificar(macr_nb_id,modifica_macro)",
+		];
+
+		grid($sql, array_keys($cols), array_values($cols), "", "", 0, "desc", 10);
+
+		rodape();
+
+	}
