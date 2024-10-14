27378bb28facc3b9ef352928acb2c77c70e22d96
Otavio ACdeC <gutsgt1@gmail.com>
2024-10-14 08:02:46 -0300

    ./contex20/sendInfoToPost.php

--- contex20/sendInfoToPost.php
+++ contex20/sendInfoToPost.php
@@ -2,3 +2,5 @@
 
     $interno = true; //Utilizado no conecta.php para reconhecer se quem está tentando acessar é uma tela ou uma query interna.
     include_once "../..".$_GET["path"]."/conecta.php";
+
+    dd("sendInfoToPost.php");
