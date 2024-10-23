01b9978c66dfddc26c7ec82c6ccf5d14a6285473
PabloDantasDev <pabloj72017@gmail.com>
2024-10-23 13:36:09 -0300

    Inserção de Rodape no arquivo logistica.php

--- armazem_paraiba/logistica.php
+++ armazem_paraiba/logistica.php
@@ -780,3 +780,8 @@ if (isset($_SESSION['user_nb_id']) && !empty($_SESSION['user_nb_id'])) {
 </script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+
+<?php
+rodape();
+
+?>
