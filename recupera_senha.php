f00789d1f88ececa17a4bdbd0ec3d04e67bb7a77
Otavio ACdeC <gutsgt1@gmail.com>
2025-04-09 11:50:10 -0300

    ./recupera_senha.php

--- recupera_senha.php
+++ recupera_senha.php
@@ -1,123 +1,55 @@
 <?php
-
-    /* Modo debug
+    //* Modo debug
 		ini_set('display_errors', 1);
 		error_reporting(E_ALL);
 	//*/
-    $interno = true;
-    global $CONTEX;
 
     include_once "./PHPMailer/src/Exception.php";
     include_once "./PHPMailer/src/PHPMailer.php";
     include_once "./PHPMailer/src/SMTP.php";
     include_once "load_env.php";
-    include_once 'empresas.php';
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-// $_POST['botao'] = '';
-// $msg = '';
-
-    function extrairEmpresa($url, $empresa_array) {
-        $parsed_url = parse_url($url);
-        $path_segments = explode('/', $parsed_url['path']);
-
-        $empresa = $path_segments[1] ?? '';
-
-        return in_array($empresa, $empresa_array) ? $empresa : null;
+    function extrairEmpresa($key, $empresas) {
+        $key = strtoupper($key);
+        return !empty($empresas[$key])? $empresas[$key]: null;
     }
-
-    if ($_POST['botao'] == 'ENVIAR') {
-        $empresa_url = $_POST['empresa'];
-        $empresa = extrairEmpresa($empresa_url, $empresa_array);
-        $login = $_POST['login'];
-        
-        if(!empty($empresa)){
-            include __DIR__.'/'.$empresa."/conecta.php";
-            
-            global $CONTEX;
-            if (!empty($login)){
-                $msg = tokenGenerate($login, $empresa);
-            }else{ 
-                $msg = 
-                    "<div class='alert alert-danger display-block'>
-                        <span> Campo E-mail ou Login não foi preenchido </span>
-                    </div>"
-                ;
-            }
-        }else{
-            $msg = 
-                "<div class='alert alert-danger display-block'>
-                    <span> Informe a sua empresa </span>
-                </div>"
-            ;
-        }
-
-    }
-
-    if ($_POST['botao'] == 'Redefinir senha') {
-        $empresa = $_GET['empresa'];
-        include $empresa."/conecta.php";
-        
-        $token = $_GET['token'];
-        $checkTokenSql = query("SELECT user_nb_id FROM user WHERE user_tx_token = '$_GET[token]'");
-        $checkToken = mysqli_fetch_assoc($checkTokenSql);
-
-        if (!isset($checkToken) && empty($checkToken)) {
-            echo '<script>alert("Link já utilizado ou invalido, por favor solicita novamente a  redefinição de senha.  ")</script>';
-            echo "<meta http-equiv='refresh' content='0; url=".$_ENV["URL_BASE"].$_ENV["APP_PATH"]."/index.php' />";
-            exit;
-        }
-        
-        if (!empty($_POST['senha']) && !empty($_POST['senha2']) && $_POST['senha'] == $_POST['senha2']) {
-                $userSql = query("SELECT user_nb_id FROM user WHERE user_tx_token = '$_GET[token]'");
-                $userId = mysqli_fetch_assoc($userSql);
-                atualizar('user', ['user_tx_senha', 'user_tx_token'], [md5($_POST['senha']), '-'], $userId['user_nb_id']);
-                $msg = "
-                <div id='redefinido' style='background-color: #0af731; padding: 1px; text-align: center;'>
-                    <h4 style = 'color: #fff !important;'>Senha Redefinida.</h4>
-                </div>";
-        } else {
-            $msg = '
-            <div id="erro" style="background-color: red; padding: 1px; text-align: center;">
-                <h4>Confirmação de senha incorreta</h4>
-            </div>';
-        }
-    }
-
 
     function tokenGenerate($login, $domain) {
         $token = bin2hex(random_bytes(16));
 
-        $userSql = query("SELECT user_nb_id, user_tx_nome, user_tx_email FROM user WHERE user_tx_login = '$login' AND user_tx_status = 'ativo'");
-        $userId = mysqli_fetch_assoc($userSql);
-        if(!empty($userId)){
-            atualizar('user', ['user_tx_token'], [$token], $userId['user_nb_id']);
-            return sendEmail($userId['user_tx_email'], $token, $userId['user_tx_nome'], $domain);
-        } else
-            return '
-            <div id="erro" style="background-color: red; padding: 1px; text-align: center;">
-                <h4 style="color: white;"><strong> Informações não estão corretas </strong></h4>
-            </div>';
+        $user = mysqli_fetch_assoc(query(
+            "SELECT user_nb_id, user_tx_nome, user_tx_email FROM user
+                WHERE user_tx_status = 'ativo'
+                    AND user_tx_login = '{$login}';"
+        ));
+        if(!empty($user)){
+            atualizar("user", ["user_tx_token"], [$token], $user["user_nb_id"]);
+            return sendEmail($user["user_tx_email"], $token, $user["user_tx_nome"], $domain);
+        }else{
+            return 
+                "<div id='erro' style='background-color: red; padding: 1px; text-align: center;'>
+                    <h4 style='color: white;'><strong> Usuário não encontrado </strong></h4>
+                </div>";
+        }
     }
 
-    function obscureEmail($email) {
-        list($user, $domain) = explode('@', $email);
+    function obscureEmail(string $email): string{
+        [$user, $domain] = explode("@", $email);
         $userLength = strlen($user);
-        $obscureLength = ceil($userLength * 0.8); // Calcula 80% do comprimento do usuário
-        $visibleLength = $userLength - $obscureLength;
+        $obscureLength = ceil($userLength*0.8); // Calcula 80% do comprimento do usuário
+        $visibleLength = $userLength-$obscureLength;
     
         // Cria a parte obscurecida do usuário
-        $obscuredUser = substr($user, 0, $visibleLength).str_repeat('*', $obscureLength);
+        $obscuredUser = substr($user, 0, $visibleLength).str_repeat("*", $obscureLength);
     
-        return $obscuredUser.'@'.$domain;
+        return "{$obscuredUser}@{$domain}";
     }
 
 
     function sendEmail($destinatario, $token, $nomeDestinatario, $domain) {
-        global $CONTEX;
-        
         $caminho = getcwd();
 
         $mail = new PHPMailer(true);
@@ -144,43 +76,184 @@
             // Conteúdo
             $mail->isHTML(true);
             $mail->Subject = 'Redefinição de Senha';
-            $mail->Body = '<b>Redefinição de Senha</b><br>
-            Por favor, <a href="'.$_ENV["URL_BASE"].'/'.basename($caminho).'/recupera_senha.php?empresa='.$domain.'&token='.$token.'">clique aqui</a> para resetar sua senha.<br>
-            Caso você não tenha solicitado este e-mail de redefinição de senha, por favor, <a href="mailto:suporte@techps.com.br ">entre em contato</a> para que possamos resolver o problema.';
-            $mail->Encoding = 'base64';
-            $mail->AltBody = "Link para recupera senha: ".$_ENV["URL_BASE"].'/'.basename($caminho)."/recupera_senha.php?token=".$token;
+            $mail->Body = 
+                "<b>Redefinição de Senha</b>
+                <br>
+                Clique <a href='{$_ENV["URL_BASE"]}/".basename($caminho)."/recupera_senha.php?empresa={$domain}&token={$token}'>aqui</a> para resetar sua senha.
+                <br>
+                Caso você não tenha solicitado este e-mail de redefinição de senha, por favor, <a href='mailto:suporte@techps.com.br'>entre em contato</a> para que possamos resolver o problema."
+            ;
+            $mail->Encoding = "base64";
+            $mail->AltBody = "Link para recuperar senha: {$_ENV["URL_BASE"]}/".basename($caminho)."/recupera_senha.php?token={$token}";
 
             $obscuredEmail = obscureEmail($destinatario);
-            if ($mail->send()) {
-                return "
-                <div id='enviado' style='text-align: center;'>
-                    <h4>E-mail enviado para $obscuredEmail</h4>
-                </div>";
+            if($mail->send()){
+                return 
+                    "<div id='enviado' style='text-align: center;'>
+                        <h4>E-mail enviado para {$obscuredEmail}</h4>
+                    </div>";
             }
-        } catch (Exception $exception) {
-            return "
-            <div id='erro' style='text-align: center;'>
-                <h4>Erro ao enviar e-mail: {$mail->ErrorInfo}</h4>
-            </div>";
+        }catch(Exception $e){
+            return 
+                "<div id='erro' style='text-align: center;'>
+                    <h4>Erro ao enviar e-mail: {$mail->ErrorInfo}</h4>
+                </div>
+                <script>console.log('{$e->getMessage()}')</script>";
         }
     }
 
-    if(!empty($_POST["appRequest"])){
-        echo $msg;
-        exit;
+    function index(string $msg = ""){
+
+        include "empresas.php";
+
+        if(!empty($_GET["empresa"])){
+            $interno = true; //Utilizado em conecta.php;
+            include __DIR__."/{$_GET["empresa"]}/conecta.php";
+        }
+
+        if (!empty($_POST["botao"])){
+            $empresa = extrairEmpresa($_POST["empresa"], $empresas);
+            
+            
+            if($_POST["botao"] == "ENVIAR"){
+                if(!empty($empresa)){
+                    if (!empty($_POST["login"])){
+                        $token = bin2hex(random_bytes(16));
+    
+                        $user = mysqli_fetch_assoc(query(
+                            "SELECT user_nb_id, user_tx_nome, user_tx_email FROM user
+                                WHERE user_tx_status = 'ativo'
+                                    AND user_tx_login = '{$_POST["login"]}';"
+                        ));
+    
+                        if(!empty($user)){
+                            atualizar("user", ["user_tx_token"], [$token], $user["user_nb_id"]);
+                            $msg = sendEmail($user["user_tx_email"], $token, $user["user_tx_nome"], $empresa);
+                        }else{
+                            $msg = 
+                                "<div id='erro' style='background-color: red; padding: 1px; text-align: center;'>
+                                    <h4 style='color: white;'><strong> Usuário não encontrado </strong></h4>
+                                </div>";
+                        }
+                    }else{ 
+                        $msg = 
+                            "<div class='alert alert-danger display-block'>
+                                <span> Campo E-mail ou Login não foi preenchido </span>
+                            </div>"
+                        ;
+                    }
+                }else{
+                    $msg = 
+                        "<div class='alert alert-danger display-block'>
+                            <span> Informe a sua empresa </span>
+                        </div>"
+                    ;
+                }
+            }elseif($_POST["botao"] == "Redefinir senha"){
+                $user = mysqli_fetch_assoc(query(
+                    "SELECT user_nb_id FROM user 
+                        WHERE user_tx_status = 'ativo' 
+                            AND user_tx_token = '{$_GET["token"]}';"
+                ));
+                
+                if(!empty($_POST["senha"]) && !empty($_POST["senha2"]) && $_POST["senha"] == $_POST["senha2"]){
+                        atualizar("user", ["user_tx_senha", "user_tx_token"], [md5($_POST["senha"]), "-"], $user["user_nb_id"]);
+                        $msg = 
+                            "<div id='redefinido' style='background-color:rgb(0, 125, 21); padding: 1px; text-align: center;'>
+                                <h4 style = 'color: #fff !important;'>Senha Redefinida. Voltando para o login...</h4>
+                            </div>"
+                        ;
+                }else{
+                    $msg = 
+                        "<div id='erro' style='background-color: red; padding: 1px; text-align: center;'>
+                            <h4>Confirmação de senha incorreta</h4>
+                        </div>"
+                    ;
+                }
+            }
+        }
+        
+
+        if(!empty($_POST["appRequest"])){
+            echo $msg;
+            exit;
+        }
+
+        if(empty($_GET["token"])){
+
+            //Utilizado em recupera_senha_html.php
+            $domainDiv = 
+                "<div id='no-domain-selected'>
+                        <h3 class='form-title font-green'></h3>
+                            <p style='text-align:justify'>Um link de redefinição de senha será enviado para o seu endereço de e-mail.</p>
+                            {$empresasInput}
+                            <div class='form-group'>
+                                <label class='control-label visible-ie8 visible-ie9'>Login</label>
+                                <input focus autofocus class='form-control form-control-solid placeholder-no-fix' type='text' autocomplete='off' placeholder='Login' name='login' />
+                            </div>
+                            ".(!empty($msg)?
+                                "{$msg}
+                                <style>
+                                    #enviar{
+                                        display:none;
+                                    }
+                                </style>":
+                                "")
+                            ."<div class='form-actions'>
+                                <a href='index.php' style='align-content: center; padding-right: 20px;'>Voltar</a>
+                                <input type='submit' class='btn green uppercase' name='botao' value='ENVIAR'>
+                            </div>
+                    </div>"
+            ;
+        }else{
+
+            $user = mysqli_fetch_assoc(query(
+                "SELECT user_nb_id FROM user 
+                    WHERE user_tx_status = 'ativo' 
+                        AND user_tx_token = '{$_GET["token"]}';"
+            ));
+            if(empty($user)){
+                echo 
+                    "<script>alert('Link já utilizado ou inválido, por favor solicite novamente a redefinição de senha.')</script>
+                    <meta http-equiv='refresh' content='0; url={$_ENV["URL_BASE"]}{$_ENV["APP_PATH"]}/index.php' />"
+                ;
+                exit;
+            }
+            unset($user);
+            
+            
+            //Utilizado em recupera_senha_html.php
+            $domainDiv = 
+                "<div id='domain-selected'>
+					<h3 class='form-title font-green'></h3>
+					<div class='form-group'>
+						<label class='control-label visible-ie8 visible-ie9'>Senha</label>
+						<input focus autofocus class='form-control form-control-solid placeholder-no-fix' type='password' autocomplete='off' placeholder='Senha' name='senha' />
+					</div>
+
+					<div class='form-group'>
+						<label class='control-label visible-ie8 visible-ie9'>Confirmar Senha</label>
+						<input focus autofocus class='form-control form-control-solid placeholder-no-fix' type='password' autocomplete='off' placeholder='Confirmar Senha' name='senha2' />
+					</div>
+					".($msg?? "")."
+					<div class='form-actions'>
+						<input type='submit' class='btn green uppercase' name='botao' value='Redefinir senha'></input>
+					</div>
+				</div>"
+            ;
+        }
+
+        include "recupera_senha_html.php";
+        echo 
+            "<script>
+                document.getElementById('logo').src = '{$_ENV["URL_BASE"]}{$_ENV["APP_PATH"]}/contex20/img/logo.png';
+                document.getElementsByClassName('form-title font-green')[0].innerHTML = '".(empty($_GET["token"])?
+                    "Redefinir Senha":
+                    "Redefinição de Senha<br>{$empresasNomes[$_GET["empresa"]]}")."';
+            </script>"
+        ;
     }
 
-    include "recupera_senha_html.php";
-
-    echo 
-        "<script>
-            document.getElementById('logo').src = '".$_ENV["URL_BASE"].$_ENV["APP_PATH"]."/contex20/img/logo.png';
-            if(".(empty($_GET['token'])? "1": "0")."){
-                document.getElementById('no-domain-selected').hidden = false;
-                document.getElementsByClassName('form-title font-green')[0].innerHTML = 'Redefinir Senha';
-            }else{
-                document.getElementById('domain-selected').hidden = false;
-                document.getElementsByClassName('form-title font-green')[1].innerHTML = 'Redefinição de Senha - {$_GET["empresa"]}';
-            }
-        </script>"
-    ;
+if(empty($_POST) || (!empty($_POST["acao"]) && $_POST["acao"] == "index")){
+    index();
+}
