6c7f763c5bcd3fdb91692f82bc240732e5748a2c
Otavio ACdeC <gutsgt1@gmail.com>
2025-04-30 10:48:36 -0300

    Adição da classe para estilizar

--- contex20/funcoes.php
+++ contex20/funcoes.php
@@ -850,7 +850,7 @@
 
 	function texto($nome,$modificador,$tamanho='',$extra=''){//Campo de texto que não pode ser editado
 		$campo =
-			'<div class="col-sm-'.$tamanho.' margin-bottom-5 campo-fit-content" '.$extra.'>
+			'<div class="col-sm-'.$tamanho.' margin-bottom-5 campo-fit-content text-field" '.$extra.'>
 				<label>'.$nome.'</label><br>
 				<p class="text-left">'.$modificador.'</p>
 			</div>';
