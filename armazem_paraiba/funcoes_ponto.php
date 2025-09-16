1f0e1eb2430aa259ad169f74eebafb2e27a1c635
Otavio ACdeC <gutsgt1@gmail.com>
2025-09-16 09:33:42 -0300

    color antes de ícone

--- armazem_paraiba/funcoes_ponto.php
+++ armazem_paraiba/funcoes_ponto.php
@@ -719,7 +719,7 @@
 			//Consultar feriados do dia{
 				$stringFeriado = getFeriados($motorista, $data);
 				if(!empty($stringFeriado)){
-					$iconeFeriado = " <a><i style='color:green;' title='{$stringFeriado}' class='fa fa-info-circle color_green'></i></a>";
+					$iconeFeriado = " <a><i style='color:green;' title='{$stringFeriado}' class='color_green fa fa-info-circle'></i></a>";
 					$aRetorno["diaSemana"] .= $iconeFeriado;
 				}
 			//}
@@ -777,7 +777,7 @@
 			if(count($pontosDia) == 0){
 				$aRetorno["diffSaldo"] = getSaldoDiario($jornadaPrevista, "00:00");
 				if(strip_tags($aRetorno["jornadaPrevista"]) != "00:00" && empty($abonos)){
-					$aRetorno["inicioJornada"][] = "<a><i style='color:red;' title='Batida início de jornada não registrada!' class='fa fa-warning color_red'></i></a>";
+					$aRetorno["inicioJornada"][] = "<a><i style='color:red;' title='Batida início de jornada não registrada!' class='color_red fa fa-warning'></i></a>";
 				}
 
 				//Converter array em string{
@@ -881,7 +881,7 @@
 					}
 					$modifyParam = explode(":", $totalIntervalo);
 					$repousosPorEspera["totalIntervalo"] = $repousosPorEspera["totalIntervalo"]->modify("{$modifyParam[0]} hours {$modifyParam[1]} minutes");
-					$repousosPorEspera["icone"] = montarIconeIntervalo($repousosPorEspera["pares"], "fa fa-info-circle color_greenLight", "#00ff00");
+					$repousosPorEspera["icone"] = montarIconeIntervalo($repousosPorEspera["pares"], "color_greenLight fa fa-info-circle", "#00ff00");
 					$registros["repousoPorEspera"] = $repousosPorEspera;
 					
 					//Adicionar os pares em $registros["repousoCompleto"]["pares"]
@@ -1011,7 +1011,7 @@
 						if(operarHorarios([$totalIntersticio, $interMinimo], "-")[0] != "-"){
 							$title .= "\nInterstício remanescente compensado com intervalos do dia (".$totalNaoJornada->format("H:i").").";
 						}
-						$icone .= "<a><i style='color:red;' title='{$title}' class='fa fa-warning color_red color_red'></i></a>";
+						$icone .= "<a><i style='color:red;' title='{$title}' class='color_red fa fa-warning'></i></a>";
 					}
 					unset($restante);
 
@@ -1120,11 +1120,11 @@
 
 		//ALERTAS{
 			if(empty($registros["inicioJornada"][0]) && $aRetorno["jornadaPrevista"] != "00:00"){
-				$aRetorno["inicioJornada"][] 	= "<a><i style='color:red;' title='Batida início de jornada não registrada!' class='fa fa-warning color_red'></i></a>";
+				$aRetorno["inicioJornada"][] 	= "<a><i style='color:red;' title='Batida início de jornada não registrada!' class='color_red fa fa-warning'></i></a>";
 			}
 			if($fezJorMinima || count($registros["inicioJornada"]) > 0){
 				if(!isset($registros["fimJornada"][0]) || $registros["fimJornada"][0] == ""){
-					$aRetorno["fimJornada"][] 	  = "<a><i style='color:red;' title='Batida fim de jornada não registrada!' class='fa fa-warning color_red color_red'></i></a>";
+					$aRetorno["fimJornada"][] 	  = "<a><i style='color:red;' title='Batida fim de jornada não registrada!' class='color_red fa fa-warning'></i></a>";
 				}
 
 				//01:00 DE REFEICAO{
@@ -1139,20 +1139,20 @@
 
 					$avisoRefeicao = "";
 					if($maiorRefeicao > "02:00"){
-						$avisoRefeicao = "<a><i style='color:orange;' title='Refeição com tempo máximo de 02:00h não respeitado.' class='fa fa-info-circle color_orange'></i></a>";
+						$avisoRefeicao = "<a><i style='color:orange;' title='Refeição com tempo máximo de 02:00h não respeitado.' class='color_orange fa fa-info-circle'></i></a>";
 					}elseif($dtJornada > $dtJornadaMinima && $maiorRefeicao < '01:00'){
-						$avisoRefeicao = "<a><i style='color:red;' title='Refeição ininterrupta maior do que 01:00h não respeitado.' class='fa fa-warning color_red color_red'></i></a>";
+						$avisoRefeicao = "<a><i style='color:red;' title='Refeição ininterrupta maior do que 01:00h não respeitado.' class='color_red fa fa-warning'></i></a>";
 					}
 				//}
 
 				// if((!isset($registros["inicioRefeicao"][0]) || empty($aRetorno["inicioRefeicao"][0])) && $jornadaEfetiva > "06:00"){
-				// 	$aRetorno["inicioRefeicao"][] = "<a><i style='color:red;' title='Batida início de refeição não registrada!' class='fa fa-warning color_red'></i></a>";
+				// 	$aRetorno["inicioRefeicao"][] = "<a><i style='color:red;' title='Batida início de refeição não registrada!' class='color_red fa fa-warning'></i></a>";
 				// }else{
 				// 	$aRetorno["inicioRefeicao"][] = $avisoRefeicao;
 				// }
 
 				// if((!isset($registros["fimRefeicao"][0]) || empty($aRetorno["fimRefeicao"][0])) && ($jornadaEfetiva > "06:00")){
-				// 	$aRetorno["fimRefeicao"][] 	  = "<a><i style='color:red;' title='Batida fim de refeição não registrada!' class='fa fa-warning color_red'></i></a>";
+				// 	$aRetorno["fimRefeicao"][] 	  = "<a><i style='color:red;' title='Batida fim de refeição não registrada!' class='color_red fa fa-warning'></i></a>";
 				// }else{
 				// 	$aRetorno["fimRefeicao"][] = $avisoRefeicao;
 				// }
@@ -1463,7 +1463,7 @@
 		return $endossoCompleto;
 	}
 
-	function montarIconeIntervalo(array $pares, string $classe = "fa fa-info-circle color_green", string $cor = "green"): string{
+	function montarIconeIntervalo(array $pares, string $classe = "color_green fa fa-info-circle", string $cor = "green"): string{
 		$icone = "";
 		$tooltip = "";
 		for($f = 0; $f < count($pares); $f++){
@@ -1725,7 +1725,7 @@
 		$datetime2 = new DateTime("2000-01-01 ".$limite);
 
 		if($datetime1 > $datetime2){
-			return "<a style='white-space: nowrap;'><i style='color:orange;' title='Tempo excedido de ".$limite."' class='fa fa-warning color_red color_orange'></i></a>&nbsp;".$tempoEfetuado;
+			return "<a style='white-space: nowrap;'><i style='color:orange;' title='Tempo excedido de ".$limite."' class='color_orange fa fa-warning'></i></a>&nbsp;".$tempoEfetuado;
 		}else{
 			return $tempoEfetuado;
 		}
@@ -1749,7 +1749,7 @@
 
 		if($mdc > "05:30"){
 			$mdc = "<a style='white-space: nowrap;'>".
-						"<i style='color:orange;' title='{$baseErrMsg}' class='fa fa-warning color_red color_orange'></i>".
+						"<i style='color:orange;' title='{$baseErrMsg}' class='color_orange fa fa-warning'></i>".
 					"</a>".
 					"&nbsp;".$mdc
 			;
@@ -1794,7 +1794,7 @@
 	
 			if($somaTempoAtivo > $tempoTrabalho && $somaTempoDescanso < $tempoDescanso){
 				$mdc = "<a style='white-space: nowrap;'>".
-							"<i style='color:orange;' title='{$baseErrMsg}\n\nDirigido: ".$somaTempoAtivo."\nDescansado: ".$somaTempoDescanso."' class='fa fa-warning color_red color_orange'></i>".
+							"<i style='color:orange;' title='{$baseErrMsg}\n\nDirigido: ".$somaTempoAtivo."\nDescansado: ".$somaTempoDescanso."' class='color_orange fa fa-warning'></i>".
 						"</a>".
 						"&nbsp;".$mdc
 				;
