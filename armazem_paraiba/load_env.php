8b51292e1ab040115fe834089a4d321fd7deb4d2
PabloDantasDev <pabloj72017@gmail.com>
2025-12-02 20:19:08 -0300

    Atualização load_env aceitar ' e sem aspas

--- armazem_paraiba/load_env.php
+++ armazem_paraiba/load_env.php
@@ -6,6 +6,13 @@
         $env = parse_ini_file($envFilePath, false, INI_SCANNER_RAW);
         foreach($env as $attr=>$val){
             $v = is_string($val) ? trim($val) : $val;
+            if(is_string($v) && strlen($v) >= 2){
+                $first = $v[0];
+                $last = $v[strlen($v)-1];
+                if(($first === "'" && $last === "'") || ($first === '"' && $last === '"')){
+                    $v = substr($v, 1, -1);
+                }
+            }
             putenv("{$attr}={$v}");
             $_ENV[$attr] = $v;
         }
