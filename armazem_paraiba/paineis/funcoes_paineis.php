8f7f7f1bdab15885e6141461759e569623d71b46
PabloDantasDev <pabloj72017@gmail.com>
2025-12-24 10:38:16 -0300

    Atualização logica de data de admissao

--- armazem_paraiba/paineis/funcoes_paineis.php
+++ armazem_paraiba/paineis/funcoes_paineis.php
@@ -861,7 +861,11 @@ function relatorio_nao_conformidade_juridica(int $idEmpresa) {
 			LEFT JOIN grupos_documentos ON  grup_nb_id = enti_setor_id
 			LEFT JOIN sbgrupos_documentos ON  sbgr_nb_id = enti_subSetor_id
 			WHERE enti_nb_empresa = {$idEmpresa}
-				AND enti_tx_dataCadastro <= '{$periodoInicio->format("Y-m-t")}'
+				AND (
+					enti_tx_admissao <= '{$periodoInicio->format("Y-m-t")}'
+					OR (enti_tx_admissao = '' AND enti_tx_dataCadastro <= '{$periodoInicio->format("Y-m-t")}')
+					OR (enti_tx_admissao IS NULL AND enti_tx_dataCadastro <= '{$periodoInicio->format("Y-m-t")}')
+				)
 				AND (
 					enti_tx_status = 'ativo'
 					OR enti_tx_desligamento > '{$periodoInicio->format("Y-m-t")}'
@@ -890,6 +894,7 @@ function relatorio_nao_conformidade_juridica(int $idEmpresa) {
 	}
 
 	$totalEmpresa = 0;
+	$motoristaTotais = [];
 	foreach ($motoristas as $motorista) {
 		$diasComProblema = [];
 
