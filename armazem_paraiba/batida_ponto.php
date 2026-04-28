16a7b4a8dc2ec0a1b674d56b76030a8a6431b075
Ornilio Neto <orniliooficial@gmail.com>
2026-04-28 09:06:04 -0300

    Correcao no icone para terceirizados

--- armazem_paraiba/batida_ponto.php
+++ armazem_paraiba/batida_ponto.php
@@ -415,15 +415,17 @@ function index() {
 		$jornadaEfetiva = operarHorarios([$jornadaCompleta, $jornadaEfetiva], "-");
 
 
+		$iconeJornada = "fa fa-flag-checkered fa-6";
+
 		$botoes = [
-            "inicioJornada"             => criaBotaoRegistro("btn green", 1,  "JORNADA", ($_SESSION["user_tx_nivel"] == "Funcionário" ? "fa fa-flag-checkered fa-6" : "fa fa-car fa-6")),
+            "inicioJornada"             => criaBotaoRegistro("btn green", 1,  "JORNADA", $iconeJornada),
             "inicioRefeicao"             => criaBotaoRegistro("btn green", 3,  "REFEIÇÃO", "fa fa-cutlery fa-6"),
             // "inicioEspera"             => criaBotaoRegistro("btn green", 5,  "ESPERA", "fa fa-clock-o fa-6"),
             "inicioDescanso"             => criaBotaoRegistro("btn green", 7,  "DESCANSO", "fa fa-hourglass-start fa-6"),
             // "inicioRepouso"             => criaBotaoRegistro("btn green", 9,  "REPOUSO", "fa fa-bed fa-6"),
             // "inicioRepousoEmbarcado"    => criaBotaoRegistro("btn green", 11, "Iniciar Repouso Embarcado", "fa fa-bed fa-6"),
 
-            "fimJornada"                 => criaBotaoRegistro("btn red", 2,  "FIM JORNADA", ($_SESSION["user_tx_nivel"] == "Funcionário" ? "fa fa-flag-checkered fa-6" : "fa fa-car fa-6")),
+			"fimJornada"                 => criaBotaoRegistro("btn red", 2,  "FIM JORNADA", $iconeJornada),
             "fimRefeicao" 				=> criaBotaoRegistro("btn red", 4,  "FIM REFEIÇÃO", "fa fa-cutlery fa-6"),
             // "fimEspera" 				=> criaBotaoRegistro("btn red", 6,  "FIM ESPERA", "fa fa-clock-o fa-6"),
             "fimDescanso" 				=> criaBotaoRegistro("btn red", 8,  "FIM DESCANSO", "fa fa-hourglass-end fa-6"),
