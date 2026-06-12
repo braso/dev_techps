6092d73c4469358dfddb693c24071ed61c22b424
PabloDantasDev <pabloj72017@gmail.com>
2026-06-12 09:14:31 -0300

    Fix macro

--- armazem_paraiba/cadastro_macro.php
+++ armazem_paraiba/cadastro_macro.php
@@ -15,40 +15,38 @@
 		exit;
 	}
 
-	// function cadastra_macro(){
+	function cadastra_macro(){
 
-	// 	$camposObrig = [
-	// 		"nome" => "Nome", 
-	// 		"codigoInterno" => "Código Interno", 
-	// 		"codigoExterno" => "Código Externo",
-	// 		"fonte" => "Fonte",
-	// 		"status" => "Status"
-	// 	];
-	// 	$errorMsg = conferirCamposObrig($camposObrig, $_POST);
-	// 	if(!empty($errorMsg)){
-	// 		set_status("ERRO: ".$errorMsg);
-	// 		layout_macro();
-	// 		exit;
-	// 	}
+		$camposObrig = [
+			"nome" => "Nome", 
+			"codigoInterno" => "Código Interno", 
+			"codigoExterno" => "Código Externo"
+		];
+		$errorMsg = conferirCamposObrig($camposObrig, $_POST);
+		if(!empty($errorMsg)){
+			set_status("ERRO: ".$errorMsg);
+			layout_macro();
+			exit;
+		}
 
-	// 	$novaMacro = [
-	// 		"macr_tx_nome" => $_POST["nome"],
-	// 		"macr_tx_codigoInterno" => $_POST["codigoInterno"],
-	// 		"macr_tx_codigoExterno" => $_POST["codigoExterno"],
-	// 		"macr_nb_user" => $_SESSION["user_nb_id"],
-	// 		"macr_tx_data" => date("Y-m-d"),
-	// 		"macr_tx_status" => "ativo"
-	// 	];
+		$novaMacro = [
+			"macr_tx_nome" => $_POST["nome"],
+			"macr_tx_codigoInterno" => $_POST["codigoInterno"],
+			"macr_tx_codigoExterno" => $_POST["codigoExterno"],
+			"macr_nb_user" => $_SESSION["user_nb_id"],
+			"macr_tx_data" => date("Y-m-d"),
+			"macr_tx_status" => "ativo"
+		];
 
-	// 	if(!empty($_POST["id"])){
-	// 		atualizar("macroponto",array_keys($novaMacro), array_values($novaMacro),$_POST["id"]);
-	// 	}else{
-	// 		inserir("macroponto",array_keys($novaMacro), array_values($novaMacro));
-	// 	}
+		if(!empty($_POST["id"])){
+			atualizar("macroponto",array_keys($novaMacro), array_values($novaMacro),$_POST["id"]);
+		}else{
+			inserir("macroponto",array_keys($novaMacro), array_values($novaMacro));
+		}
 
-	// 	index();
-	// 	exit;
-	// }
+		index();
+		exit;
+	}
 
 
 	function layout_macro(){
@@ -56,13 +54,13 @@
 		cabecalho("Cadastro Macro");
 
 		$c = [
-			texto("Nome*",($_POST["macr_tx_nome"]?? ""),5),
-			texto("Código Interno*",($_POST["macr_tx_codigoInterno"]?? ""),3),
-			texto("Código Externo*",($_POST["macr_tx_codigoExterno"]?? ""),3)
+			campo("Nome*","nome",($_POST["macr_tx_nome"]?? ""),5),
+			campo("Código Interno*","codigoInterno",($_POST["macr_tx_codigoInterno"]?? ""),3),
+			campo("Código Externo*","codigoExterno",($_POST["macr_tx_codigoExterno"]?? ""),3)
 		];
 
 		$botao = [
-			// botao("Gravar","cadastra_macro","id",$_POST["id"],"","","btn btn-success"),
+			botao("Gravar","cadastra_macro","id",$_POST["id"]?? "","","","btn btn-success"),
 			criarBotaoVoltar()
 		];
 		
@@ -91,7 +89,7 @@
 		$botoes = [
 			botao("Buscar","index"),
 			botao("Limpar Filtro","limparFiltros"),
-			// botao("Inserir","layout_macro")
+			botao("Inserir","layout_macro")
 		];
 		
 		echo abre_form();
